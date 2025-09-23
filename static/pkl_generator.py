e9fdaf953776e05bfae18ab085d6dd3eeafa7024
CesarConstanty <cesarpdhthesis@gmail.com>
2025-09-23 19:00:02 +0200

    nouveaux layouts et pkl stuff

--- static/pkl_generator.py
+++ static/pkl_generator.py
@@ -6,11 +6,11 @@ from overcooked_ai_py.mdp.overcooked_mdp import OvercookedGridworld
 from overcooked_ai_py.planning.planners import MotionPlanner
 
 layouts = [
-    "Ldd18_R04_V00","Ldd18_R11_V00","Ldd18_R27_V00","Ldd18_R71_V00","Ldd19_R54_V00","Ldd19_R64_V00","Ldd19_R82_V00","Ldd21_R75_V00","Ldd22_R03_V00","Ldd22_R29_V00","Ldd22_R69_V00","Ldd22_R78_V00",
-    "Ldd18_R04_V00_R90","Ldd18_R11_V00_R90","Ldd18_R27_V00_R90","Ldd18_R71_V00_R90","Ldd19_R54_V00_R90","Ldd19_R64_V00_R90","Ldd19_R82_V00_R90","Ldd21_R75_V00_R90","Ldd22_R03_V00_R90","Ldd22_R29_V00_R90","Ldd22_R69_V00_R90","Ldd22_R78_V00_R90",
-    "Ldd18_R04_V00_R180","Ldd18_R11_V00_R180","Ldd18_R27_V00_R180","Ldd18_R71_V00_R180","Ldd19_R54_V00_R180","Ldd19_R64_V00_R180","Ldd19_R82_V00_R180","Ldd21_R75_V00_R180","Ldd22_R03_V00_R180","Ldd22_R29_V00_R180","Ldd22_R69_V00_R180","Ldd22_R78_V00_R180"
+    "test01", "test02", "test03", "test04", "test05", "test06", "test07", "test08", "test09", "test10", "test11", "test12",
+    "test01_90", "test02_90", "test03_90", "test04_90", "test05_90", "test06_90", "test07_90", "test08_90", "test09_90", "test10_90", "test11_90", "test12_90",
+    "test01_180", "test02_180", "test03_180", "test04_180", "test05_180", "test06_180", "test07_180", "test08_180", "test09_180", "test10_180", "test11_180", "test12_180"
 ]
-layouts_dir = "overcooked_ai_py/data/layouts/generation_cesar"
+layouts_dir = "overcooked_ai_py/data/layouts/generation_cesar_2"
 
 for layout in layouts:
     print(f"Processing {layout}")
